682c7068575837f74d9b664fd6062807741d1d89
Victor Chang <victor.chang.1024@gmail.com>
2013-11-17 21:57:51 -0800

    Fixed to work better.

--- belaynets/robot.py
+++ belaynets/robot.py
@@ -5,21 +5,29 @@ class Robot:
     def act(self, game):
         # Goal: Leave the spawn point and/or attack any adjacent enemy.
         
-        # if there are adjacent enemies, attack one.
+        print "bot at", self.location
+        
+        # if there are enemies around, attack them
         for loc, bot in game.get('robots').items():
-            if bot.get('player_id') != self.player_id:
-                # enemy bot
+            if bot.player_id != self.player_id:
                 if rg.dist(loc, self.location) <= 1:
-                    # bot is adjacent
+                    print "attack", loc
                     return ['attack', loc]
             
         if 'spawn' in rg.loc_types(self.location):
             # get possible move locations
-            move_locs = rg.locs_around(self.location, filter_out=['obstacle', 'invalid'])
+            move_locs = rg.locs_around(self.location, filter_out=('invalid', 'obstacle'))
+            
+            for loc, bot in game.get('robots').items():
+                if loc in move_locs:
+                    print "removing", loc, "from", move_locs
+                    move_locs.remove(loc)
             
             move = rg.toward(self.location, rg.CENTER_POINT)
-            if ('obstacle' in rg.loc_types(move) or 'invalid' in rg.loc_types(move)) and len(move_locs) > 0:
+            print "default", move, "out of", move_locs
+            if move not in move_locs:
                 move = move_locs[random.randint(0, len(move_locs)-1)]
+            print "move", move
             return ['move', move]
             
         return ['guard']
