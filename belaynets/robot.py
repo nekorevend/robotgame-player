3ee335fc49830304c4cdfacb3f742835c87d6fb9
Victor Chang <victor.chang.1024@gmail.com>
2013-11-17 17:29:29 -0800

    First try on a passive-aggressive approach.

--- belaynets/robot.py
+++ belaynets/robot.py
@@ -2,15 +2,43 @@ import rg
 
 class Robot:
     def act(self, game):
-        # if we're in the center, stay put
-        if self.location == rg.CENTER_POINT:
-            return ['guard']
-
-        # if there are enemies around, attack them
+        # Goal: Stay away from the enemy. If no direction to escape, then attack.
+        
+        # get possible move locations
+        move_locs = rg.locs_around(self.location, filter_out=['spawn', 'obstacle', 'invalid'])
+        adjacent_bots = []
+        
+        # if there are enemies adjacent, try to escape.
         for loc, bot in game.get('robots').items():
             if bot.get('player_id') != self.player_id:
+                # enemy bot
+                # if enemy bot is adjacent to a move location, remove that location from possibilities.
+                for move in move_locs:
+                    if rg.dist(loc, move) <= 2:
+                        move_locs.remove(move)
+                        
                 if rg.dist(loc, self.location) <= 1:
-                    return ['attack', loc]
+                    # bot is adjacent
+                    adjacent_bots.append((loc, bot))
+        
+        if len(adjacent_bots) > 0:
+            # there is an adjacent enemy
+            # try to run
+            if len(move_locs) > 0:
+                return ['move', move_locs[0]]   
+            # couldn't run, attack weakest enemy
+            lowest_hp = 50
+            attack_loc = None
+            for loc, bot in adjacent_bots:
+                if lowest_hp > bot.get('hp'):
+                    lowest_hp = bot.get('hp')
+                    attack_loc = loc
+            if attack_loc:
+                return ['attack', attack_loc]
 
-        # move toward the center
-        return ['move', rg.toward(self.location, rg.CENTER_POINT)]
+        # nothing's going on
+        if 'spawn' in rg.loc_types(self.location):
+            return ['move', rg.toward(self.location, rg.CENTER_POINT)]
+        else:
+            return ['guard']
+            
