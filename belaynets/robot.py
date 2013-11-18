db44569066c4eb42487caa067f34418a6378caa1
Victor Chang <victor.chang.1024@gmail.com>
2013-11-18 00:36:33 -0800

    Begin testing syntax for global variable to use to organize movement of bots.

--- belaynets/robot.py
+++ belaynets/robot.py
@@ -1,33 +1,46 @@
 import rg
 import random
 
+move_intents = []
+    
 class Robot:
+    
     def act(self, game):
         # Goal: Leave the spawn point and/or attack any adjacent enemy.
         
-        print "bot at", self.location
+        # avoid bots moving into each other
+        global move_intents
+        # print "intents", move_intents
+        
+        # print "bot at", self.location
+        
+        spawn_move = True if 'spawn' in rg.loc_types(self.location) else False
         
         # if there are enemies around, attack them
         for loc, bot in game.get('robots').items():
             if bot.player_id != self.player_id:
                 if rg.dist(loc, self.location) <= 1:
-                    print "attack", loc
+                    # print "attack", loc
                     return ['attack', loc]
-            
-        if 'spawn' in rg.loc_types(self.location):
+            else:
+                if 'spawn' in rg.loc_types(loc) and rg.dist(loc, self.location) <= 2:
+                    spawn_move = True
+        
+        if spawn_move:
             # get possible move locations
             move_locs = rg.locs_around(self.location, filter_out=('invalid', 'obstacle'))
             
             for loc, bot in game.get('robots').items():
                 if loc in move_locs:
-                    print "removing", loc, "from", move_locs
+                    # print "removing", loc, "from", move_locs
                     move_locs.remove(loc)
             
             move = rg.toward(self.location, rg.CENTER_POINT)
-            print "default", move, "out of", move_locs
-            if move not in move_locs:
-                move = move_locs[random.randint(0, len(move_locs)-1)]
-            print "move", move
+            # print "default", move, "out of", move_locs
+            if move not in move_locs and len(move_locs) > 0:
+                move = random.choice(move_locs)
+            # print "move", move
+            move_intents.append(move)
             return ['move', move]
             
         return ['guard']
